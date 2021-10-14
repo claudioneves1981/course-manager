ef5e034ec27d85483a7563b8f5b73b84601edd00
Claudio Francisco das Neves <cfneguacu@uni9.edu.br>
2021-10-14 15:48:06 -0300

    Projeto Course Manager Bootcamp Angular Santander

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
+import { RouterModule } from '@angular/router';
+import { Error404Component } from './core/component/error-404/erro-404.component';
+import { HttpClientModule} from '@angular/common/http';
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([
+      {
+        path: '',redirectTo: 'courses', pathMatch: 'full'
+      }
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
